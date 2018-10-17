e908e2c9ce4a2376ff74e8a997fcfb6480f27d2c
Alex <12884007@student.uts.edu.au>
2018-10-17 12:17:19 +1100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    image= models.FileField (upload_to='post_image', blank=True)
     title = models.CharField(max_length=200)
-    subhead = models.CharField(max_length=100, blank=True, null=True)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
